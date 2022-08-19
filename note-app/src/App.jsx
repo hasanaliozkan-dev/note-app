5566044e9ee71f04ea0702e0e08e1677af7836e4
hasanaliozkan-dev <hasanaliozkan@posta.mu.edu.tr>
2022-08-19 11:16:22 +0300

    "project finished"

--- note-app/src/App.jsx
+++ note-app/src/App.jsx
@@ -9,12 +9,12 @@ import {nanoid} from "nanoid"
 
 export default function App() {
     const [notes, setNotes] = React.useState( 
-      JSON.parse(localStorage.getItem("notes")) || [])
+      () => JSON.parse(localStorage.getItem("notes")) || [])
 
     const [currentNoteId, setCurrentNoteId] = React.useState(
         (notes[0] && notes[0].id) || ""
   )
-  
+
     
     React.useEffect(() => {
       localStorage.setItem("notes", JSON.stringify(notes))
@@ -29,13 +29,27 @@ export default function App() {
     }
     
     function updateNote(text) {
-        setNotes(oldNotes => oldNotes.map(oldNote => {
-            return oldNote.id === currentNoteId
-                ? { ...oldNote, body: text }
-                : oldNote
-        }))
+        setNotes(oldNotes => {
+          const newArray =[]
+          for(let i = 0; i < oldNotes.length; i++) {
+            const oldNote = oldNotes[i]
+            if(oldNote.id === currentNoteId) {
+              newArray.unshift({...oldNote, body: text})
+            }else{
+              newArray.push(oldNote)
+            }
+          }
+          
+          return newArray
+        })
     }
     
+    function deleteNote(event,noteId){
+          event.stopPropagation()
+          setNotes(oldNotes => oldNotes.filter(note => note.id !== noteId))
+          
+
+    }
     function findCurrentNote() {
         return notes.find(note => {
             return note.id === currentNoteId
@@ -57,6 +71,7 @@ export default function App() {
                     currentNote={findCurrentNote()}
                     setCurrentNoteId={setCurrentNoteId}
                     newNote={createNewNote}
+                    deleteNote = {deleteNote}
                 />
                 {
                     currentNoteId && 
